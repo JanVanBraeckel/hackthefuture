cf0ad0dc4b771a240bb7c1f954155cb279bd5708
Annemie De Groote <miebeertje@hotmail.com>
2015-12-03 12:56:14 +0100

    layout

--- HackTheFuture/app/src/main/java/com/example/gebruiker/hackthefuture/LoginActivity.java
+++ HackTheFuture/app/src/main/java/com/example/gebruiker/hackthefuture/LoginActivity.java
@@ -1,44 +1,23 @@
 package com.example.gebruiker.hackthefuture;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.example.gebruiker.hackthefuture.REST.services.UserManager;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -50,6 +29,11 @@ public class LoginActivity extends AppCompatActivity {
     EditText mEmailView;
     @Bind(R.id.password)
     EditText mPasswordView;
+    @Bind(R.id.logoApp)
+    ImageView imageView;
+    @Bind(R.id.loginScreen)
+    LinearLayout login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +42,28 @@ public class LoginActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+
         userManager = UserManager.getInstance(getApplicationContext());
+
+        int logo_app = getResources().getIdentifier("logo_app","drawable", getPackageName());
+
+
+        Glide.with(this)
+                .load(logo_app)
+                .centerCrop()
+                .override(350,350)
+                .into(imageView);
+
+        Glide.with(getApplicationContext())
+                .load(R.drawable.vault1)
+                .asBitmap()
+                .into(new SimpleTarget<Bitmap>(this.getResources().getDisplayMetrics().widthPixels, this.getResources().getDisplayMetrics().heightPixels) {
+                    @Override
+                    public void onResourceReady(Bitmap bitmap, GlideAnimation anim) {
+                        BitmapDrawable background = new BitmapDrawable(bitmap);
+                        login.setBackgroundDrawable(background);
+                    }
+                });
     }
 
     @OnClick(R.id.sign_in)
